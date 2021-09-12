2fff730d081ce675b30f1b4b48748fc073b6e631
koebe1 <koe.be.pay@web.de>
2021-09-12 18:05:25 +0200

    added removing items

--- src/App.js
+++ src/App.js
@@ -1,64 +1,7 @@
 import React from "react";
+import ToDo from "./components/todo";
+
 import "./App.css";
-
-function List(props) {
-  const list = props.list;
-
-  return (
-    <ul>
-      {list.map((listItem, index) => {
-        return (
-          <li key={index} className="listItem">
-            {listItem}
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-class ToDo extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      toDoList: [],
-      inputValue: ""
-    };
-  }
-
-  handleInputChange = event => {
-    this.setState({
-      inputValue: event.target.value
-    });
-  };
-
-  handleAdding = () => {
-    this.setState({
-      toDoList: [...this.state.toDoList, this.state.inputValue],
-      inputValue: ""
-    });
-  };
-  render() {
-    const { inputValue, toDoList } = this.state;
-    return (
-      <div className="to-do-container">
-        <input
-          value={inputValue}
-          onChange={this.handleInputChange}
-          type="text"
-        />
-        <button
-          onClick={this.handleAdding}
-          disabled={inputValue === "" ? true : false}
-        >
-          Add
-        </button>
-        <List list={toDoList} />
-      </div>
-    );
-  }
-}
 
 function App() {
   return (
